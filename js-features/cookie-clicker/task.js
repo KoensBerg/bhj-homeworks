e30165b4a07a1d71bbeb253d94ad4d50d866bba7
Oleg Yanke <yanke.kd@gmail.com>
2023-08-21 11:51:19 +0200

    first commit cookie-clicker

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -5,11 +5,11 @@
 const clickerCounter = document.getElementById("clicker__counter");
 const cookieImg = document.getElementById("cookie");
 
-cookieImg.onclick = function() {
+cookieImg.onclick = function () {
   clickerCounter.textContent++;
   if (clickerCounter.textContent % 2) {
-    cookieImg.width="300";
+    cookieImg.width = "300";
   } else {
-    cookieImg.width="200";
+    cookieImg.width = "200";
   }
 }
