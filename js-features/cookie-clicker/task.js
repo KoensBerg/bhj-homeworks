3c577733692ff157cfe3c595ec66b7642eb7252c
Oleg Yanke <yanke.kd@gmail.com>
2023-08-21 13:09:50 +0200

    commit cookie-clicker level two

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -2,12 +2,36 @@
 // Задача 2 -- Игра-кликер
 // — — — — — — — — — — — — — — — — — — — —
 
+// Найдём существующие html-элементы
 const clickerCounter = document.getElementById("clicker__counter");
 const cookieImg = document.getElementById("cookie");
 
+// Создадим новый html-элемент для вывода скорости клика
+let divSpeed = document.createElement('div');
+divSpeed.className = "clicker__status";
+divSpeed.innerHTML = 'Скорость клика: <span id="clicker__speed">0</span>';
+cookieImg.before(divSpeed);
+
+const clickerSpeed = document.getElementById("clicker__speed");
+
+// Определим переменные
+let currentCounter = clickerCounter.textContent;
+let currentSpeed = clickerSpeed.textContent;
+let startTime = new Date().getTime();
+let currentTime = null;
+let fullTimeInSeconds = null;
+
+// Опишем действия по клику
 cookieImg.onclick = function () {
-  clickerCounter.textContent++;
-  if (clickerCounter.textContent % 2) {
+  currentCounter = ++clickerCounter.textContent;
+
+  currentTime = new Date().getTime();
+  fullTimeInSeconds = (currentTime - startTime) / 1000;
+  currentSpeed = fullTimeInSeconds / currentCounter;
+
+  clickerSpeed.textContent = currentSpeed.toFixed(2);
+
+  if (currentCounter % 2) {
     cookieImg.width = "300";
   } else {
     cookieImg.width = "200";
