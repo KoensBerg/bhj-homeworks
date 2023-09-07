6d2f19c68d424eeff1233f80cfca2c45e63644f1
Oleg Yanke <yanke.kd@gmail.com>
2023-09-07 20:14:08 +0200

    first commit tabs

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -2,21 +2,18 @@
 // Задача 3 – Управление вкладками
 // — — — — — — — — — — — — — — — — — — —
 
-// Предположим, что навигаций на странице может быть несколько
+// Исходим из того, что на странице может быть несколько блоков с навигацией
+
 const firstNaviTabs = Array.from(document.querySelector('#tabs1').querySelectorAll('.tab'));
 const firstNaviTabContens = Array.from(document.querySelector('#tabs1').querySelectorAll('.tab__content'));
 
-// Логика клика по вкладке
+
 for (let i = 0; i < firstNaviTabs.length; i++) {
   firstNaviTabs[i].addEventListener('click', () => {
-    getNonActive();
+    firstNaviTabs.forEach(e => e.className = 'tab');
     firstNaviTabs[i].className = 'tab tab_active';
+
+    firstNaviTabContens.forEach(e => e.className = 'tab__content');
     firstNaviTabContens[i].className = 'tab__content tab__content_active';
   });
-}
-
-// Делаем все вкладки неактивными
-function getNonActive() {
-  firstNaviTabs.forEach(e => e.className = 'tab');
-  firstNaviTabContens.forEach(e => e.className = 'tab__content');
 }
