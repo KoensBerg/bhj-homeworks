820250fc015a777c98685cc6f84ad5fc72b7829d
Oleg Yanke <yanke.kd@gmail.com>
2023-09-11 21:22:53 +0200

    first commit ads

---
+++ dom/ads/task.js
@@ -0,0 +1,20 @@
+const rotatorCases = Array.from(document.querySelectorAll('.rotator__case'));
+let counter = 1;
+let delay = null;
+let intervalID = null;
+
+function changeString() {
+  clearInterval(intervalID);
+
+  rotatorCases.forEach(e => e.classList.remove('rotator__case_active'));
+  rotatorCases[counter].classList.add('rotator__case_active');
+  rotatorCases[counter].style.color = rotatorCases[counter].dataset.color;
+
+  if (counter === (rotatorCases.length - 1)) counter = 0;  
+  else counter++;
+
+  delay = rotatorCases[counter].dataset.speed;
+  intervalID = setInterval(changeString, delay);
+}
+
+intervalID = setInterval(changeString, delay);
