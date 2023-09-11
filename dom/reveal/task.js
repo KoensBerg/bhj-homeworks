1ecf9185eebb7f1702db5c6a2c6a48ef95a952aa
Oleg Yanke <yanke.kd@gmail.com>
2023-09-11 20:00:48 +0200

    first commit reveal

---
+++ dom/reveal/task.js
@@ -0,0 +1,13 @@
+const revealElements = Array.from(document.querySelectorAll('.reveal'));
+
+for (let i = 0; i < revealElements.length; i++) {
+  window.addEventListener('scroll', () => {
+    const { top, bottom } = revealElements[i].getBoundingClientRect();
+
+    if (top < window.innerHeight && bottom < window.innerHeight)
+    revealElements[i].classList.add('reveal_active');
+    
+    if (top < 0 || bottom > window.innerHeight)
+    revealElements[i].classList.remove('reveal_active');
+  });
+}
