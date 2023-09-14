906828684a3a5668d252872a6b2141d07b7d4544
Oleg Yanke <yanke.kd@gmail.com>
2023-09-14 13:20:28 +0200

    first commit interests

---
+++ html-forms/interests/task.js
@@ -0,0 +1,11 @@
+const checkboxes = document.querySelectorAll('.interest__check');
+
+for (let i = 0; i < checkboxes.length; i++) {
+  checkboxes[i].addEventListener('change', () => {
+    const checkboxParent = checkboxes[i].closest('.interest');
+    const checkboxChildren = Array.from(checkboxParent.querySelectorAll('.interest__check'));
+    
+    if (checkboxes[i].checked) checkboxChildren.forEach(e => e.checked = true);
+    else checkboxChildren.forEach(e => e.checked = false);
+  });
+}
