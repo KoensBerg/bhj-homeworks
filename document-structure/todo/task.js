094ba05518eb468ee56116d6b382e9390cce7f14
Oleg Yanke <yanke.kd@gmail.com>
2023-09-16 22:58:42 +0200

    first commit todo

---
+++ document-structure/todo/task.js
@@ -0,0 +1,49 @@
+// — — — — — — — — — — — — — — — — — —
+// Задача 2 – Простой список дел
+// — — — — — — — — — — — — — — — — — —
+
+const taskInput = document.getElementById('task__input');
+const tasksAdd = document.getElementById('tasks__add');
+const tasksList = document.getElementById('tasks__list');
+
+// ввод задачи через Enter
+taskInput.addEventListener('keypress', (event) => {
+  if (event.keyCode === 13 && taskInput.value) createNewTask();
+});
+
+// ввод через кнопку "Добавить"
+tasksAdd.addEventListener('click', () => {
+  if (taskInput.value) createNewTask();
+});
+
+
+// — — — — — — — — — — — — — — — — — —
+// Функция создаёт новую задачу
+// — — — — — — — — — — — — — — — — — —
+
+function createNewTask() {
+  let newTask = document.createElement('div');
+
+  // создадим новую задачу
+  newTask.classList.add('task');
+  newTask.innerHTML = `
+  <div class="task__title">
+    ${taskInput.value}
+  </div>
+  <a href="#" class="task__remove">&times;</a>
+  `;
+
+  // добавим новую задачу в начало списка
+  tasksList.insertAdjacentElement('afterBegin', newTask);
+
+  // очистим поле ввода
+  taskInput.value = '';
+
+  // настроим удаление созданной задачи
+  const taskRemove = newTask.lastElementChild;
+  
+  taskRemove.addEventListener('click', (event) => {
+    event.preventDefault();
+    newTask.remove();
+  });
+}
