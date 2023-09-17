2af413465adb2bec9d0906955404a3f442e44d00
Oleg Yanke <yanke.kd@gmail.com>
2023-09-17 14:26:17 +0200

    second commit todo

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -7,17 +7,21 @@ const tasksAdd = document.getElementById('tasks__add');
 const tasksList = document.getElementById('tasks__list');
 const tasksForm = document.getElementById('tasks__form');
 
+// восстановим задачи из localStorage
+recreateTaskList();
+
 // настроим ввод новой задачи
 tasksForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  createNewTask();
+  if (taskInput.value) {
+    e.preventDefault();
+    createNewTask();
+  }
 })
 
 
 // — — — — — — — — — — — — — — — — — —
 // Функция создания новой задачи
 // — — — — — — — — — — — — — — — — — —
-
 function createNewTask() {
   const newTask = document.createElement('div');
 
@@ -33,14 +37,47 @@ function createNewTask() {
   // добавим новую задачу в начало списка
   tasksList.insertAdjacentElement('afterBegin', newTask);
 
+  // обновим localStorage
+  updateLocalStorage();
+
   // очистим поле ввода
   taskInput.value = '';
 
   // настроим удаление созданной задачи
   const taskRemove = newTask.lastElementChild;
-  
+
   taskRemove.addEventListener('click', (event) => {
     event.preventDefault();
     newTask.remove();
+    updateLocalStorage();
   });
+}
+
+// — — — — — — — — — — — — — — — — — —
+// Функция обновления localStorage
+// — — — — — — — — — — — — — — — — — —
+function updateLocalStorage() {
+  localStorage.clear();
+  localStorage.setItem('tasksList.innerHTML', tasksList.innerHTML);
+}
+
+// — — — — — — — — — — — — — — — — — —
+// Функция восстановления задач из localStorage
+// — — — — — — — — — — — — — — — — — —
+function recreateTaskList() {
+  if (localStorage['tasksList.innerHTML']) {
+    tasksList.innerHTML = localStorage['tasksList.innerHTML'];
+  }
+  
+  if (tasksList.children.length) {
+    const tasks = document.querySelectorAll('.task');
+
+    for (let i = 0; i < tasks.length; i++) {
+      tasks[i].lastElementChild.addEventListener('click', (e) => {
+        e.preventDefault();
+        tasks[i].remove();
+        updateLocalStorage();
+      });
+    }
+  }
 }
