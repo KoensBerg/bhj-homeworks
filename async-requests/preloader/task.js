a2b86c1d2f1504fd339c3dd78ddd48051cda6810
Oleg Yanke <yanke.kd@gmail.com>
2023-09-20 10:10:13 +0200

    second commit preloader

--- async-requests/preloader/task.js
+++ async-requests/preloader/task.js
@@ -1,43 +1,105 @@
+// — — — — — — — — — — — — — — — — — — — — —
+// Задача 1 – Анимация загрузки данных
+// — — — — — — — — — — — — — — — — — — — — —
+
 const imgLoader = document.getElementById('loader');
 const items = document.getElementById('items');
 
-let xhr = new XMLHttpRequest();
 
+// восстановим данные из localStorage
+recreateValutesList();
+
+
+// отправим запрос на получение свежих курсов валют
+let xhr = new XMLHttpRequest();
 xhr.open('GET', 'https://students.netoservices.ru/nestjs-backend/slow-get-courses');
 xhr.send();
 
-xhr.onload = function () {
-  imgLoader.classList.remove('loader_active');
 
+xhr.onload = function () {
+  // если от сервера получен корректный ответ
   if (xhr.status = 200) {
+    items.innerHTML = ''; // очистим старый список
+    imgLoader.classList.remove('loader_active'); // удалим индикатор загрузки
+
+    // извлечём данные о тикерах и курсах
     const valute = JSON.parse(xhr.response).response.Valute;
 
     for (let i = 0; i < Object.entries(valute).length; i++) {
       const CharCode = Object.entries(valute)[i][1].CharCode;
       const Value = Object.entries(valute)[i][1].Value;
 
-      const itemCode = document.createElement('div');
-      itemCode.classList.add('item__code');
-      itemCode.textContent = CharCode;
-
-      const itemValue = document.createElement('div');
-      itemValue.classList.add('item__value');
-      itemValue.textContent = Value;
-
-      const itemCurrency = document.createElement('div');
-      itemCurrency.classList.add('item__currency');
-      itemCurrency.textContent = 'руб.';
-
-      const item = document.createElement('div');
-      item.classList.add('item');
-
-      item.appendChild(itemCode);
-      item.appendChild(itemValue);
-      item.appendChild(itemCurrency);
-
-      items.appendChild(item);
+      // создадим свежий список курсов валют
+      createValutesList(CharCode, Value);
     }
+
+    // обновим данные в localStorage
+    updateLocalStorage();
   } else {
     console.log(`Ошибка запроса данных: ${xhr.status}`);
+  }
+}
+
+
+// — — — — — — — — — — — — — — — — — — — — —
+// Функция создания списка с курсами валют
+// — — — — — — — — — — — — — — — — — — — — —
+function createValutesList(CharCode, Value) {
+  const itemCode = document.createElement('div');
+  itemCode.classList.add('item__code');
+  itemCode.textContent = CharCode;
+
+  const itemValue = document.createElement('div');
+  itemValue.classList.add('item__value');
+  itemValue.textContent = Value;
+
+  const itemCurrency = document.createElement('div');
+  itemCurrency.classList.add('item__currency');
+  itemCurrency.textContent = 'руб.';
+
+  const item = document.createElement('div');
+  item.classList.add('item');
+
+  item.appendChild(itemCode);
+  item.appendChild(itemValue);
+  item.appendChild(itemCurrency);
+
+  items.appendChild(item);
+}
+
+
+// — — — — — — — — — — — — — — — — — —
+// Функция обновления localStorage
+// — — — — — — — — — — — — — — — — — —
+function updateLocalStorage() {
+  const exchangeRatesArray = [];
+  const valutesList = items.querySelectorAll('.item');
+
+  if (valutesList.length) {
+    for (let i = 0; i < valutesList.length; i++) {
+      const CharCode = valutesList[i].querySelector('.item__code').textContent;
+      const Value = valutesList[i].querySelector('.item__value').textContent;
+
+      exchangeRatesArray.push({ CharCode, Value });
+    }
+
+    console.log(exchangeRatesArray);
+    localStorage.setItem('exchangeRatesArray', JSON.stringify(exchangeRatesArray));
+  }
+}
+
+
+// — — — — — — — — — — — — — — — — — — — — —
+// Функция восстановления из localStorage
+// — — — — — — — — — — — — — — — — — — — — —
+function recreateValutesList() {
+  if (localStorage.exchangeRatesArray) {
+    const exchangeRatesArray = JSON.parse(localStorage.exchangeRatesArray);
+    console.log(exchangeRatesArray);
+
+    if (exchangeRatesArray.length) {
+      items.innerHTML = '';
+      Array.from(exchangeRatesArray).forEach(e => createValutesList(e.CharCode, e.Value));
+    }
   }
 }
