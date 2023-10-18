df8230c077955a5b2ff832eaba4db03dc6aa7b75
Oleg Yanke <yanke.kd@gmail.com>
2023-10-18 10:13:07 +0200

    advanced text editor

--- client-state/text-editor/task.js
+++ client-state/text-editor/task.js
@@ -3,14 +3,26 @@
 // — — — — — — — — — — — — — — — — —
 
 const editor = document.getElementById('editor');
+const reset = document.getElementById('reset');
+
 
 // восстановим данные из localStorage
 restorEditor();
+
 
 // запишем новые данные в localStorage
 editor.addEventListener('input', () => { // либо 'change' (в зависимости от задачи)
   localStorage.editor = JSON.stringify(editor.value.trim());
 });
+
+
+// очистим форму и localStorage
+reset.addEventListener('click', (e) => {
+  e.preventDefault();
+  editor.value = '';
+  delete localStorage.editor;
+});
+
 
 // Функция восстанавливает данные из localStorage
 function restorEditor() {
